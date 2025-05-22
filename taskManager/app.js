8a76a03cfe74d1477be13ad6a4540ad5491b0993
Nzioka6350 <jamesnziokamutua1@gmail.com>
2025-05-22 16:45:13 +0300

    Solved env problem in accessing database port

--- taskManager/app.js
+++ taskManager/app.js
@@ -1,10 +1,11 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
 import db from './db/dbConfig.js';
 const app = express();
 
-const port = process.env.DB_PORT || 3000 
-
-
+const port = process.env.DB_PORT || 3000;
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 app.listen(port,()=>{
     console.log("App is running at port", port);
 })
