4e01b58ff2bacd9bb9588da3bc4543ff3638cfa3
Nzioka6350 <jamesnziokamutua1@gmail.com>
2025-05-22 16:40:30 +0300

    Solved env problem in accessing database port

--- taskManager/app.js
+++ taskManager/app.js
@@ -1,9 +1,8 @@
 import express from 'express';
-import dbConfig from './db/dbConfig.js';
-import dotenv from 'dotenv'
+import db from './db/dbConfig.js';
 const app = express();
 
-const port = process.env.DB_PORT;
+const port = process.env.DB_PORT || 3000 
 
 
 app.listen(port,()=>{
--- taskManager/db/dbConfig.js
+++ taskManager/db/dbConfig.js
@@ -1,13 +1,15 @@
+import mysql2 from 'mysql2'
 import dotenv from 'dotenv';
 dotenv.config();
 
 
-const dbConfig = {
+const db = mysql2.createPool({
     host:process.env.DB_HOST,
     database: process.env.DB_NAME,
-    password: process.env.DB_HOST,
+    password: process.env.DB_PASS,
     port: process.env.DB_PORT,
 
 }
+)
 
-export default dbConfig;
+export default db;
